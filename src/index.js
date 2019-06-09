5f8e27a05b2746fabbc7c9f7feeb203373a63ec8
Chen Wilson <chenlwilson@gmail.com>
2019-06-09 15:29:22 -0700

    Handles IMG ext and return a link reference

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import myIcon from './icon.jpeg';
 
 const App = () => (
   <div>
     <div>Hello World!</div>
-    <img src='icon.png'></img>
   </div>
 )
 
